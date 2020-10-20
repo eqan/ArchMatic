a0ad726cdec1585fce1b53f4416f9be73b3f8881
eqan <61124752+eqan@users.noreply.github.com>
2020-10-20 10:22:50 +0500

    Update 2-software-pacman.sh

--- 2-software-pacman.sh
+++ 2-software-pacman.sh
@@ -11,9 +11,6 @@ echo -e "\nINSTALLING SOFTWARE\n"
 
 PKGS=(
 
-    # SYSTEM --------------------------------------------------------------
-
-    'linux-lts'             # Long term support kernel
 
     # TERMINAL UTILITIES --------------------------------------------------
 
@@ -88,7 +85,7 @@ PKGS=(
 
     # MEDIA ---------------------------------------------------------------
 
-    'celluloid'             # Video player
+    'mpv'             # Video player
     
     # GRAPHICS AND DESIGN -------------------------------------------------
     
